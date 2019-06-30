491e8b7c1c12c70442dc9ef21c771ac673fc319b
Hiram <hiramtan@live.com>
2019-06-30 19:20:31 +0800

    init project

---
+++ HiFramework/HiFramework/Binder.cs
@@ -0,0 +1,22 @@
+﻿/***************************************************************
+ * Description: 
+ *
+ * Documents: 
+ * Author: hiramtan@live.com
+***************************************************************/
+using HiFramework.Core;
+namespace HiFramework
+{
+    public class Binder:Core.Binder
+    {
+        public override void Init()
+        {
+            base.Init();
+            Bind<ISignalComponent>().To<SignalComponent>();
+            Bind<IEventComponent>().To<EventComponent>();
+            Bind<IIOComponent>().To<IOComponent>();
+            Bind<IInjectComponent>().To<InjectComponent>();
+            Bind<IObjectManager>().To<ObjectsManager>();
+        }
+    }
+}
---
+++ HiFramework/HiFramework/Extensions/Singleton.cs
@@ -0,0 +1,77 @@
+﻿//*********************************************************************
+// Description:安全单例
+// 提供两种类型的书写方式,如果类不能继承singleton<T>时,可以使用第二种方式快速提供单例
+// Author: hiramtan@live.com
+//*********************************************************************
+
+namespace HiFramework
+{
+    public class Singleton<T> where T : new()
+    {
+        private static T _instance;
+        private static readonly object Locker = new object();
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (Locker)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new T();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+
+    public static class Singleton
+    {
+        private static object _locker = new object();
+
+        public static T GetInstance<T>() where T : class, new()
+        {
+            if (InstanceStorage<T>.Instance == null)
+            {
+                lock (_locker)
+                {
+                    if (InstanceStorage<T>.Instance == null)
+                    {
+                        InstanceStorage<T>.Instance = new T();
+                    }
+                }
+            }
+            return InstanceStorage<T>.Instance;
+        }
+
+        private static class InstanceStorage<T> where T : class, new()
+        {
+            internal static volatile T Instance = default(T);
+        }
+    }
+}
+
+////example
+//public class Test_First : Singleton<Test_First>
+//{
+//    public void Do() { }
+//}
+
+//public class Test_Second
+//{
+//    public void Do() { }
+//}
+
+//public class Test1
+//{
+//    void Init()
+//    {
+//        Test_First.Instance.Do();
+//        Singleton.GetInstance<Test_Second>().Do();
+//    }
+//}
