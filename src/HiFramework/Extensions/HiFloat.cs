216e5df5dfd8e346535fd8ccd03d8926599035f5
Hiram <hiramtan@live.com>
2019-08-29 11:41:51 +0800

    modify new float and add unit test

--- src/HiFramework/Extensions/HiFloat.cs
+++ src/HiFramework/Extensions/HiFloat.cs
@@ -4,23 +4,26 @@
  * Document: https://github.com/hiramtan/HiFramework
  * Author: hiramtan@live.com
  ****************************************************************************/
- using System;
+using System;
 
 namespace HiFramework
 {
+    /// <summary>
+    /// 新float类型,自动保留小数点后两位
+    /// </summary>
+    [Serializable]
     public struct HiFloat
     {
-        private const int Length = 7;
-        private float _value;
-
         public float Value
         {
             get { return _value; }
         }
+        private float _value;
+        private const int Length = 2;
 
         private HiFloat(float value)
         {
-            _value = (float) Math.Round(value, Length);
+            _value = (float)Math.Round(value, Length);
         }
 
         public static implicit operator HiFloat(float value)
@@ -28,9 +31,34 @@ namespace HiFramework
             return new HiFloat(value);
         }
 
-        public static implicit operator float(HiFloat value)
+        public static bool operator ==(HiFloat left, HiFloat right)
         {
-            return value._value;
+            return left.Value == right.Value;
+        }
+
+        public static bool operator !=(HiFloat left, HiFloat right)
+        {
+            return left.Value != right.Value;
+        }
+
+        public static bool operator <(HiFloat left, HiFloat right)
+        {
+            return left.Value < right.Value;
+        }
+
+        public static bool operator >(HiFloat left, HiFloat right)
+        {
+            return left.Value > right.Value;
+        }
+
+        public static bool operator <=(HiFloat left, HiFloat right)
+        {
+            return left.Value <= right.Value;
+        }
+
+        public static bool operator >=(HiFloat left, HiFloat right)
+        {
+            return left.Value >= right.Value;
         }
 
         public static HiFloat operator ++(HiFloat value)
@@ -41,6 +69,16 @@ namespace HiFramework
         public static HiFloat operator --(HiFloat value)
         {
             return --value._value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is HiFloat))
+            {
+                return false;
+            }
+            var other = (HiFloat)obj;
+            return Value == other.Value;
         }
     }
 }
---
+++ src/HiFrameworkTests/Extensions/HiFloatTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HiFramework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HiFramework.Tests
+{
+    [TestClass]
+    public class HiFloatTest
+    {
+        [TestMethod]
+        public void TestFloat()
+        {
+            HiFloat x = 1;
+            HiFloat y = 1.0f;
+            HiFloat z = 1.12345f;
+
+            Assert.AreEqual(x, 1);
+            Assert.AreEqual(y, 1);
+            Assert.AreEqual(z, 1.12f);
+        }
+    }
+}
